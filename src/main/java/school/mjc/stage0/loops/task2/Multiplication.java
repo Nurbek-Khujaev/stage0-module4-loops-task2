5befe19c9e38d5dd409c2a5a32d4ef08c9983ecb
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-11-03 10:56:39 +0500

    bugs fixed

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,14 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int sum = 1;
+
         int cnt = 0;
 
         while (cnt <= multiplyByAndToInclusive) {
-            sum *= cnt;
+
+            System.out.println(cnt * multiplyByAndToInclusive);
             cnt++;
         }
 
-        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int n = chars.length;
-        String s = "";
+        String s = " ";
         int cnt = 0;
 
 
